d9ae5beb69e8f1015b83470dc1cf088927e49bba
zzinao <kjn0265@gmail.com>
2022-07-07 16:03:31 +0900

    initial commit
    set up CRA --ts

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Edit from './pages/Edit'
+import Detail from './pages/Detail'
+import Signin from './pages/Signin'
+import Add from './pages/Add'
+import NotFound from './pages/NotFound';
+import Error from './pages/Error'
+import { ErrorBoundary } from 'react-error-boundary';
+
+
+
+function App() {
+  return (
+    <ErrorBoundary FallbackComponent={Error}>
+       <BrowserRouter>
+ <Routes>
+  <Route path="/" element={<Home />}/>
+  <Route path="/edit/:id" element={<Edit />}/>
+  <Route path="/book/:id" element={<Detail />}/>
+  <Route path="/signin" element={<Signin />}/>
+  <Route path="/add" element={<Add />}/>
+  <Route path="*" element={<NotFound />}/>
+ </Routes>
+ </BrowserRouter>
+    </ErrorBoundary>
+
+  );
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Home from "./Home"
+import Edit from './Edit'
+import Add from './Add'
+import Signin from './Signin'
+import Detail from './Detail'
+import NotFound from "./NotFound"
+
+export default { Home, Edit, Add, Signin, Detail, NotFound}
